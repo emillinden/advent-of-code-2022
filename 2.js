44c543df85cc00a9a0c03f1aa00e436083f9366d
Emil Lindén <emil.linden@effectsoft.se>
2022-12-08 09:36:27 +0100

    ✌️

--- 2.js
+++ 2.js
@@ -1,26 +1,26 @@
 const fs = require("fs");
 const t = fs.readFileSync("input/2.txt", "utf-8").split("\n");
 
-const u = (a,b) => a.indexOf(b);
-const y = (a,b) => u(a,b) + 1;
+const u = (a, b) => a.indexOf(b);
+const y = (a, b) => u(a, b) + 1;
 const k = ["A", "B", "C"];
 const h = ["X", "Y", "Z"];
 
 function p1() {
     let s = 0;
-    
+
     for (let i = 0; i < t.length; i++) {
         const [a, x] = t[i].split(" ");
         const r = y(k, a) - y(h, x);
         s += (r === 0 ? 3 : r === -1 || r === 2 ? 6 : 0) + y(h, x);
     }
-    
+
     return s;
 }
 
 function p2() {
     let s = 0;
-    
+
     for (let i = 0; i < t.length; i++) {
         let [a, x] = t[i].split(" ");
         const g = x === "X" ? 0 : x === "Y" ? 3 : 6;
